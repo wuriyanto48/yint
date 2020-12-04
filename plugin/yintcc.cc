9e16e845577c664d6bb0d6072a70d8393d505386
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-12-05 00:46:08 +0700

    prototyping http get

--- plugin/yintcc.cc
+++ plugin/yintcc.cc
@@ -1,6 +1,7 @@
 #include <napi.h>
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -12,29 +13,22 @@
 
 namespace yint {
 
-const int PORT = 80;
+const char* HTTP_PORT = "80";
+const char* SECURE_HTTP_PORT = "443";
 
 char* _StringAsCharArr(std::string* str)
 {
     return str->empty() ? NULL : &*str->begin();
 }
 
-int _GetIP(const char* web_url, char** ip)
+int _GetIP(const char* web_url, char** ip, char** hostname)
 {
     hostent* he = gethostbyname(web_url);
     if (he == NULL)
     {
         return -1;
     }
-
-    int l = sizeof(he->h_addr_list)/sizeof(char*);
-    printf("%d\n", l);
-    for (int i = 0; i < l; i++)
-    {
-        char* ll = inet_ntoa(*(struct in_addr*)he->h_addr_list[i]);
-        printf("%s\n", ll);
-    }
-
+    *hostname = he->h_name;
     *ip = inet_ntoa(*(struct in_addr*)he->h_addr_list[0]);
 
     return 0;
@@ -59,7 +53,7 @@ Napi::Value GetIP(const Napi::CallbackInfo& info)
     std::string url_val = info[0].As<Napi::String>().Utf8Value();
 
     char* ip = nullptr;
-    int res_ip = _GetIP(_StringAsCharArr(&url_val), &ip);
+    int res_ip = _GetIP(_StringAsCharArr(&url_val), &ip, NULL);
     if (res_ip < 0)
     {
         Napi::TypeError::New(env, "cannot get ip").ThrowAsJavaScriptException();
@@ -89,15 +83,80 @@ Napi::Value HTTPGet(const Napi::CallbackInfo& info)
     std::string url_val = info[0].As<Napi::String>().Utf8Value();
 
     char* ip = nullptr;
-    int res_ip = _GetIP(_StringAsCharArr(&url_val), &ip);
+    char* hostname = nullptr;
+    int res_ip = _GetIP(_StringAsCharArr(&url_val), &ip, &hostname);
     if (res_ip < 0)
     {
         Napi::TypeError::New(env, "cannot get ip").ThrowAsJavaScriptException();
         return env.Null();
     }
 
-    printf("%s\n", ip);
+    addrinfo addr_i, *addr_i_p;
+    memset(&addr_i, 0, sizeof(addr_i));
 
+    addr_i.ai_family = AF_INET;
+    addr_i.ai_socktype = SOCK_STREAM;
+    addr_i.ai_flags = AI_PASSIVE;
+
+    int addr_info_res = getaddrinfo(ip, HTTP_PORT, &addr_i, &addr_i_p);
+    if (addr_info_res != 0)
+    {
+        Napi::TypeError::New(env, gai_strerror(addr_info_res)).ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if (addr_i_p == NULL)
+    {
+        Napi::TypeError::New(env, "error: no address found").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    // create new socket
+    int sock_FD = socket(addr_i_p->ai_family, addr_i_p->ai_socktype, addr_i_p->ai_protocol);
+    if (sock_FD == -1) 
+    {
+        Napi::TypeError::New(env, "error: creating socket").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    // estabilish new TCP connection
+    int connect_R = connect(sock_FD, addr_i_p->ai_addr, addr_i_p->ai_addrlen);
+    if (connect_R == -1) 
+    {
+        close(sock_FD);
+        Napi::TypeError::New(env, "error: connecting socket").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    printf("%s\n", "---------");
+    printf("%s\n", ip);
+    printf("%s\n", hostname);
+    printf("%d\n", sock_FD);
+
+    std::ostringstream msg;
+    msg << "GET / HTTP/1.1\r\n"
+        << "Host: " << hostname << "\r\n"
+        << "Accept: text/html\r\n"
+        << "Connection: close\r\n"
+        << "\r\n\r\n";
+
+    std::string msg_request = msg.str();
+
+    int sent = send(sock_FD, msg_request.c_str(), msg_request.length(), 0);
+    if (sent == -1) 
+    {
+        Napi::TypeError::New(env, "error: sending request").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    char rec;
+    while(read(sock_FD, &rec, 1) > 0)
+    {
+        std::cout << rec;
+    }
+
+    close(sock_FD);
+    freeaddrinfo(addr_i_p);
     Napi::String ret_val = Napi::String::New(env, "fu*k");
     return ret_val;
 }
